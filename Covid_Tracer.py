dc482c582d3fec1b24ea668a2e114102eaaa5a82
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-07-18 11:06:27 +0800

    [FNL_OOP] Added Instruction Guide for Program

--- Covid_Tracer.py
+++ Covid_Tracer.py
@@ -1,1 +1,9 @@
 #Name: MENDIOLA, LOGIE | Year & Section: BSCPE 1-5 | Object Oriented Programming | Finals Project
+
+#Instructions:
+#Create “your” own covid contact tracing app with GUI
+#Create a program that ask user for typical information found in covid contact tracing app
+#Write the collected information in a file (use any format you like)
+#The app should be able to add and search entry
+#Please don’t follow any online contact tracing programming tutorial
+
